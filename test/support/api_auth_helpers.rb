26e88aa3c10f2ed38d8dbc12a3a5db52e9f289d2
Shawn Carrick <carrickshawn@gmail.com>
2026-06-18 17:13:27 -0400

    tests: Use DTA #create_new_auth_token method to log users in for API integration tests. This speeds tests up ~30% over making the login API call.

--- test/support/api_auth_helpers.rb
+++ test/support/api_auth_helpers.rb
@@ -1,10 +1,17 @@
 module ApiAuthHelpers
   AUTH_TOKEN_HEADERS = [ "access-token", "client", "uid", "expiry", "token-type" ].freeze
 
-  def auth_headers_for(user, password: "password123")
-    post "/auth/sign_in", params: { email: user.email, password: password }, as: :json
+  def auth_headers_for(user)
+    user.create_new_auth_token
+  end
+
+  def sign_in_headers_for(user, password: "password123")
+    post "/auth/sign_in",
+      params: { email: user.email, password: password },
+      as: :json
+
     assert_response :success
 
-    AUTH_TOKEN_HEADERS.index_with { |header| response.headers[header] }
+    response.headers.slice(*AUTH_TOKEN_HEADERS)
   end
 end
