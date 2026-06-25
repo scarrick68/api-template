5abb372ef33a08bf526b94db75fc410ad6237d4d
Shawn Carrick <carrickshawn@gmail.com>
2026-06-25 16:50:13 -0400

    feat: Add an admin tools index page for easy viewing of all available operational tools in one place.

---
+++ app/controllers/admin_tools_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class AdminToolsController < ApplicationController
+  FRIENDLY_NAMES = {
+    "/admin/tools" => "Admin Dashboard",
+    "/avo" => "Avo",
+    "/pghero" => "PgHero",
+    "/blazer" => "Blazer",
+    "/jobs" => "Mission Control Jobs",
+    "/solid_errors" => "Solid Errors",
+    "/field_test" => "Field Test",
+    "/flipper" => "Flipper",
+    "/searchjoy" => "Searchjoy",
+    "/letter_opener" => "Letter Opener"
+  }.freeze
+
+  def index
+    @tool_links = tool_paths.map { |path| [ friendly_name(path), path ] }
+  end
+
+  private
+
+  def tool_paths
+    paths = Rails.application.routes.routes.filter_map do |route|
+      normalized_path = normalize_path(route.path.spec.to_s)
+      next unless normalized_path
+      next unless FRIENDLY_NAMES.key?(normalized_path)
+
+      normalized_path
+    end
+
+    paths.uniq.sort_by { |path| path == "/admin/tools" ? "0" : path }
+  end
+
+  def normalize_path(raw_path)
+    clean = raw_path.to_s.sub(/\(.*/, "")
+    return if clean.blank?
+
+    segments = clean.delete_prefix("/").split("/")
+    return if segments.empty?
+
+    return "/admin/tools" if segments.first == "admin" && segments[1] == "tools"
+
+    "/#{segments.first}"
+  end
+
+  def friendly_name(path)
+    FRIENDLY_NAMES[path] || path.delete_prefix("/").tr("_", " ").split.map(&:capitalize).join(" ")
+  end
+end
---
+++ test/integration/admin_tools_dashboard_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class AdminToolsDashboardTest < ApplicationDispatchTest
+  test "dashboard renders expected admin tool links" do
+    sign_in create(:admin), scope: :admin
+
+    get "/admin/tools"
+
+    assert_response :success
+    assert_select "h1", "Admin Dashboard"
+    assert_select "a[href='/admin/tools']", text: "Admin Dashboard"
+
+    assert_select "a[href='/avo']", text: "Avo"
+    assert_select "a[href='/pghero']", text: "PgHero"
+    assert_select "a[href='/blazer']", text: "Blazer"
+    assert_select "a[href='/jobs']", text: "Mission Control Jobs"
+    assert_select "a[href='/solid_errors']", text: "Solid Errors"
+    assert_select "a[href='/field_test']", text: "Field Test"
+    assert_select "a[href='/flipper']", text: "Flipper"
+    assert_select "a[href='/searchjoy']", text: "Searchjoy"
+  end
+end
