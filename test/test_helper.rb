026e719f1dff11c178230602349a8c8c69da0287
Shawn Carrick <carrickshawn@gmail.com>
2026-06-28 15:03:15 -0400

    gate coverage reporting behind COVERAGE env flag. This reduces noise during dev and runs full coverage with bin/ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,26 +1,30 @@
-ENV["RAILS_ENV"] ||= "test"
+COVERAGE_ENABLED = ENV.fetch("COVERAGE", "false").casecmp("true").zero?
 
-require "simplecov"
-require "fileutils"
+if COVERAGE_ENABLED
+  require "simplecov"
+  require "fileutils"
 
-FileUtils.mkdir_p(File.expand_path("../tmp", __dir__)) unless File.exist?(File.expand_path("../tmp", __dir__))
+  FileUtils.mkdir_p(File.expand_path("../tmp", __dir__)) unless File.exist?(File.expand_path("../tmp", __dir__))
 
-SimpleCov.enable_coverage :branch
-SimpleCov.coverage_dir "coverage"
-SimpleCov.merge_timeout 3600
+  SimpleCov.enable_coverage :branch
+  SimpleCov.coverage_dir "coverage"
+  SimpleCov.merge_timeout 3600
 
-SimpleCov.start "rails" do
-  add_filter "/test/"
-  add_filter "/config/"
-  add_filter "/vendor/"
-  add_filter "/docs/"
+  SimpleCov.start "rails" do
+    add_filter "/test/"
+    add_filter "/config/"
+    add_filter "/vendor/"
+    add_filter "/docs/"
 
-  add_group "Models", "app/models"
-  add_group "Controllers", "app/controllers"
-  add_group "Jobs", "app/jobs"
-  add_group "Services", "app/services"
-  minimum_coverage line: 80, branch: 80
+    add_group "Models", "app/models"
+    add_group "Controllers", "app/controllers"
+    add_group "Jobs", "app/jobs"
+    add_group "Services", "app/services"
+    minimum_coverage line: 80, branch: 80
+  end
 end
+
+ENV["RAILS_ENV"] ||= "test"
 
 require_relative "../config/environment"
 require "rails/test_help"
@@ -28,7 +32,11 @@ require "mocha/minitest"
 require "skooma"
 
 path_to_openapi = Rails.root.join("docs", "openapi.yml")
-ActionDispatch::IntegrationTest.include Skooma::Minitest[path_to_openapi, coverage: :report]
+if COVERAGE_ENABLED
+  ActionDispatch::IntegrationTest.include Skooma::Minitest[path_to_openapi, coverage: :report]
+else
+  ActionDispatch::IntegrationTest.include Skooma::Minitest[path_to_openapi]
+end
 
 Dir[Rails.root.join("test/support/**/*.rb")].sort.each do |file|
   require file
@@ -41,14 +49,14 @@ module ActiveSupport
 
     parallelize_setup do |worker|
       Searchkick.index_suffix = worker
-      SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+      SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}" if COVERAGE_ENABLED
 
       # reindex models for parallel tests
       User.reindex
     end
 
     parallelize_teardown do |worker|
-      SimpleCov.result
+      SimpleCov.result if COVERAGE_ENABLED
     end
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
