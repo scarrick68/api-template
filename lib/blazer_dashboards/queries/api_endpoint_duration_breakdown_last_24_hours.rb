a8c596e22e42b327a04f4c9420b60c912e49b40e
Shawn Carrick <carrickshawn@gmail.com>
2026-06-28 19:46:11 -0400

    feat: Update default included Blazer dashboards to include rich breakdown of API requests with total, app compute, db and view duration data

---
+++ lib/blazer_dashboards/queries/api_endpoint_duration_breakdown_last_24_hours.rb
@@ -0,0 +1,27 @@
+module BlazerDashboards
+  module Queries
+    class ApiEndpointDurationBreakdownLast24Hours
+      def self.sql
+        <<~SQL
+          select
+            labels->>'controller' as controller,
+            labels->>'action' as action,
+            avg(value) filter (where name = 'observability.api.request.duration.app_compute_ms') as app_compute_ms,
+            avg(value) filter (where name = 'observability.api.request.duration.db_ms') as db_ms,
+            avg(value) filter (where name = 'observability.api.request.duration.view_ms') as view_ms,
+            avg(value) filter (where name = 'observability.api.request.duration_ms') as total_ms
+          from metrics
+          where occurred_at >= now() - interval '24 hours'
+            and name in (
+              'observability.api.request.duration_ms',
+              'observability.api.request.duration.app_compute_ms',
+              'observability.api.request.duration.db_ms',
+              'observability.api.request.duration.view_ms'
+            )
+          group by 1, 2
+          order by total_ms desc nulls last
+        SQL
+      end
+    end
+  end
+end
---
+++ lib/blazer_dashboards/queries/api_request_duration_breakdown_last_6_hours.rb
@@ -0,0 +1,26 @@
+module BlazerDashboards
+  module Queries
+    class ApiRequestDurationBreakdownLast6Hours
+      def self.sql
+        <<~SQL
+          select
+            date_trunc('minute', occurred_at) as minute,
+            avg(value) filter (where name = 'observability.api.request.duration.app_compute_ms') as app_compute_ms,
+            avg(value) filter (where name = 'observability.api.request.duration.db_ms') as db_ms,
+            avg(value) filter (where name = 'observability.api.request.duration.view_ms') as view_ms,
+            avg(value) filter (where name = 'observability.api.request.duration_ms') as total_ms
+          from metrics
+          where occurred_at >= now() - interval '6 hours'
+            and name in (
+              'observability.api.request.duration_ms',
+              'observability.api.request.duration.app_compute_ms',
+              'observability.api.request.duration.db_ms',
+              'observability.api.request.duration.view_ms'
+            )
+          group by 1
+          order by 1
+        SQL
+      end
+    end
+  end
+end
